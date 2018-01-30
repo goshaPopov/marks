803f3453c59ae216725223914f473eaaec8b5f3d
g.popov <gorge39@gmail.com>
2018-01-31 00:02:38 +0500

    init

--- src/main/java/ru/popov/marktodo/controllers/UserController.java
+++ src/main/java/ru/popov/marktodo/controllers/UserController.java
@@ -2,11 +2,13 @@ package ru.popov.marktodo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UserController {
 
-    @RequestMapping("/user")
+
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
     public String greeting(){
         return "Hi! Goga!";
     }
